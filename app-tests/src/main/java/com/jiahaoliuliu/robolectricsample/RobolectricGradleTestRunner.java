d2d822f4c638f190318ee83c14a1bf20bef32f91
Jiahao Liu <jiahao.liu@dubizzle.com>
2015-02-08 20:50:21 +0400

    Improved the Gradle runner

--- app-tests/src/main/java/com/jiahaoliuliu/robolectricsample/RobolectricGradleTestRunner.java
+++ app-tests/src/main/java/com/jiahaoliuliu/robolectricsample/RobolectricGradleTestRunner.java
@@ -9,37 +9,34 @@ import org.robolectric.res.Fs;
 public class RobolectricGradleTestRunner extends RobolectricTestRunner {
     private static final int MAX_SDK_SUPPORTED_BY_ROBOLECTRIC = 18;
 
+    // Since the Android manifest is in another module, System.getProperty
+    // for "android.manifest", "android.resources" and "android.assets" returns null,
+    // so it is not possible use them.
+    // Instead of that, the directory are hard coded
+    private static final String APP_ROOT_DIRECTORY = "../app/src/main/";
+    private static final String ANDROID_MANIFEST_FILE_NAME = "AndroidManifest.xml";
+    private static final String RES_DIR = "res";
+    private static final String ASSETS_DIR = "assets";
+
     public RobolectricGradleTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
     }
 
     @Override
     protected AndroidManifest getAppManifest(Config config) {
-        //String appRoot = "D:\\TmpCodingProjects\\TripComputer\\app\\src\\main\\";
-        String appRoot = "../app/src/main/";
-        String manifestPath = appRoot + "AndroidManifest.xml";
-        String resDir = appRoot + "res";
-        String assetsDir = appRoot + "assets";
-        AndroidManifest manifest = createAppManifest(Fs.fileFromPath(manifestPath),
+
+        String manifestPath = APP_ROOT_DIRECTORY + ANDROID_MANIFEST_FILE_NAME;
+        String resDir = APP_ROOT_DIRECTORY + RES_DIR;
+        String assetsDir = APP_ROOT_DIRECTORY + ASSETS_DIR;
+        AndroidManifest manifest = new AndroidManifest(Fs.fileFromPath(manifestPath),
                 Fs.fileFromPath(resDir),
-                Fs.fileFromPath(assetsDir));
-
-        // If you change the package - don't forget to change the build.gradle and the AndroidManifest.xml
-        manifest.setPackageName("com.jiahaoliuliu.robolectricsample");
-        // Robolectric is already going to look in the  'app' dir ...
-        // so no need to add to package name
-        return manifest;
-    }
-
-    /*@Override
-    protected AndroidManifest getAppManifest(Config config) {
-        String manifestProperty = "../app/src/main/AndroidManifest.xml";
-        String resProperty = "../app/src/main/res";
-        return new AndroidManifest(Fs.fileFromPath(manifestProperty), Fs.fileFromPath(resProperty)) {
+                Fs.fileFromPath(assetsDir)) {
             @Override
             public int getTargetSdkVersion() {
                 return MAX_SDK_SUPPORTED_BY_ROBOLECTRIC;
             }
         };
-    }*/
+
+        return manifest;
+    }
 }
