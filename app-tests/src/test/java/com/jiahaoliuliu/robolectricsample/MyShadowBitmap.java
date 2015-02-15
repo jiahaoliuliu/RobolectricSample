52cec0f3ae1effb97565086cca62debd1bb40db4
Jiahao Liu <jiahao.liu@dubizzle.com>
2015-02-15 19:02:28 +0400

    Added the missing Shadow Bitmap class

---
+++ app-tests/src/test/java/com/jiahaoliuliu/robolectricsample/MyShadowBitmap.java
@@ -0,0 +1,32 @@
+package com.jiahaoliuliu.robolectricsample;
+
+import android.graphics.Bitmap;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+import org.robolectric.shadows.ShadowBitmap;
+
+import java.io.OutputStream;
+
+/**
+ * Created by jiahao on 2/15/15.
+ */
+@Implements(Bitmap.class)
+public class MyShadowBitmap extends ShadowBitmap {
+    @RealObject
+    private Bitmap realBitmap;
+    private int bitmapQuality = -1;
+
+    @Implementation
+    public boolean compress(Bitmap.CompressFormat format, int quality, OutputStream stream) {
+        bitmapQuality = quality;
+        System.out.println("Using the shadow to compress");
+        return true;
+//        return realBitmap.compress(format, quality, stream);
+    }
+
+    public int getQuality() {
+        return bitmapQuality;
+    }
+}
