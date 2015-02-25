0da96c5d3ef62de08cc84b9c15d63ec01fc07d03
Jiahao Liu <jiahao.liu@dubizzle.com>
2015-02-25 19:19:25 +0400

    Created StaggeredGridView Activity and test

---
+++ app-tests/src/test/java/com/jiahaoliuliu/robolectricsample/StaggeredGridViewActivityTest.java
@@ -0,0 +1,29 @@
+package com.jiahaoliuliu.robolectricsample;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+import static org.assertj.android.api.Assertions.assertThat;
+
+/**
+ * Created by jiahao on 2/25/15.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+public class StaggeredGridViewActivityTest {
+
+    private StaggeredGridViewActivity mStaggeredGridViewActivity;
+
+    @Before
+    public void setup() {
+        // Create the class
+        mStaggeredGridViewActivity = Robolectric.buildActivity(StaggeredGridViewActivity.class).create().get();
+    }
+
+    @Test
+    public void testLinkedViews() {
+        // Check if the activity is not null
+        assertThat(mStaggeredGridViewActivity).isNotNull();
+    }
+}
